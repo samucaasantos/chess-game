5476b5734dbba1262e3b3578a0e415aa6f0c97d7
samucaasantos <sads200528@gmail.com>
2026-08-28 20:35:49 -0300

    feat: class Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
     static void main() {
-
+        Position position = new Position(3, 5);
+        System.out.println(position);
     }
 }
