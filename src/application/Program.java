83dcdddebaf4bfa674b3706846f5e6f898caa6d1
samucaasantos <sads200528@gmail.com>
2026-08-28 23:00:54 -0300

    feat: Chess Layer and printBoard method

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,10 @@
 package application;
 
-import boardgame.Board;
+import chess.ChessMatch;
 
 public class Program {
     static void main() {
-        Board board = new Board(8, 8);
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
     }
 }
